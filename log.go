d005b51ccf5889a2b3178e0ab5d7af564b9650c0
BobWong <15959187562@qq.com>
2020-04-06 19:40:17 +0800

    feat: go log update

--- log.go
+++ log.go
@@ -13,9 +13,9 @@
 package golog
 
 import (
-	"fmt"
 	"io"
-	"os"
+	"runtime"
+	"strings"
 	"sync"
 )
 
@@ -31,6 +31,8 @@ type Logger struct {
 	level       Level
 	enableColor bool
 	name        string
+	pkgName     string
+	userData    interface{}
 	colorFile   *ColorFile
 
 	parts []PartFunc
@@ -41,7 +43,7 @@ type Logger struct {
 	currLevel     Level
 	currText      string
 	currCondition bool
-	enableShow    bool
+	currContext   interface{}
 }
 
 // New creates a new Logger.   The out variable sets the
@@ -51,33 +53,32 @@ type Logger struct {
 
 const lineBuffer = 32
 
+func getPackageName() string {
+	pc, _, _, _ := runtime.Caller(2)
+	raw := runtime.FuncForPC(pc).Name()
+	return strings.TrimSuffix(raw, ".init.ializers")
+}
 func New(name string) *Logger {
+
 	l := &Logger{
 		level:         Level_Debug,
 		name:          name,
-		output:        os.Stdout,
+		pkgName:       getPackageName(),
 		buf:           make([]byte, 0, lineBuffer),
 		currCondition: true,
-		enableShow: true,
 	}
 
-	l.SetParts(LogPart_Level, LogPart_Name, LogPart_Time)
+	l.SetParts(LogPart_CurrLevel, LogPart_Name, LogPart_Time)
 
 	add(l)
 
 	return l
 }
 
-func (self *Logger) SetEnable(enable bool) {
-	self.enableShow = enable
-}
-
-func (self *Logger) SetOutptut(writer io.Writer) {
-	self.output = writer
-}
-
 func (self *Logger) EnableColor(v bool) {
+	self.mu.Lock()
 	self.enableColor = v
+	self.mu.Unlock()
 }
 
 func (self *Logger) SetParts(f ...PartFunc) {
@@ -85,6 +86,11 @@ func (self *Logger) SetParts(f ...PartFunc) {
 	self.parts = []PartFunc{logPart_ColorBegin}
 	self.parts = append(self.parts, f...)
 	self.parts = append(self.parts, logPart_Text, logPart_ColorEnd, logPart_Line)
+}
+
+func (self *Logger) SetFullParts(f ...PartFunc) {
+
+	self.parts = f
 }
 
 // 二次开发接口
@@ -96,37 +102,49 @@ func (self *Logger) WriteRawByte(b byte) {
 	self.buf = append(self.buf, b)
 }
 
+func (self *Logger) WriteRawByteSlice(b []byte) {
+	self.buf = append(self.buf, b...)
+}
+
 func (self *Logger) Name() string {
 	return self.name
 }
 
-func (self *Logger) selectColorByLevel() {
-
-	if levelColor := colorFromLevel(self.currLevel); levelColor != NoColor {
-		self.currColor = levelColor
-	}
-
+func (self *Logger) SetUserData(data interface{}) {
+	self.userData = data
 }
 
-func (self *Logger) selectColorByText() {
-
-	if self.enableColor && self.colorFile != nil && self.currColor == NoColor {
-		self.currColor = self.colorFile.ColorFromText(self.currText)
-	}
-
-	return
+func (self *Logger) UserData() interface{} {
+	return self.userData
 }
 
-func (self *Logger) Log(level Level, text string) {
-	if !self.enableShow {
-		return
-	}
+func (self *Logger) PkgName() string {
+	return self.pkgName
+}
+
+func (self *Logger) Buff() []byte {
+	return self.buf
+}
+
+// 仅供LogPart访问
+func (self *Logger) Text() string {
+	return self.currText
+}
+
+// 仅供LogPart访问
+func (self *Logger) Context() interface{} {
+	return self.currContext
+}
+
+func (self *Logger) LogText(level Level, text string, ctx interface{}) {
+
 	// 防止日志并发打印导致的文本错位
 	self.mu.Lock()
 	defer self.mu.Unlock()
 
 	self.currLevel = level
 	self.currText = text
+	self.currContext = ctx
 
 	defer self.resetState()
 
@@ -143,7 +161,12 @@ func (self *Logger) Log(level Level, text string) {
 		p(self)
 	}
 
-	self.output.Write(self.buf)
+	if self.output != nil {
+		self.output.Write(self.buf)
+	} else {
+		globalWrite(self.buf)
+	}
+
 }
 
 func (self *Logger) Condition(value bool) *Logger {
@@ -158,73 +181,9 @@ func (self *Logger) Condition(value bool) *Logger {
 func (self *Logger) resetState() {
 	self.currColor = NoColor
 	self.currCondition = true
+	self.currContext = nil
 }
 
-func (self *Logger) SetColor(name string) *Logger {
-	self.mu.Lock()
-	self.currColor = matchColor(name)
-	self.mu.Unlock()
-
-	return self
-}
-
-func (self *Logger) Debugf(format string, v ...interface{}) {
-
-	self.Log(Level_Debug, fmt.Sprintf(format, v...))
-}
-
-func (self *Logger) Infof(format string, v ...interface{}) {
-
-	self.Log(Level_Info, fmt.Sprintf(format, v...))
-}
-
-func (self *Logger) Warnf(format string, v ...interface{}) {
-
-	self.Log(Level_Warn, fmt.Sprintf(format, v...))
-}
-
-func (self *Logger) Errorf(format string, v ...interface{}) {
-
-	self.Log(Level_Error, fmt.Sprintf(format, v...))
-}
-
-func (self *Logger) Debugln(v ...interface{}) {
-
-	self.Log(Level_Debug, fmt.Sprintln(v...))
-}
-
-func (self *Logger) Infoln(v ...interface{}) {
-
-	self.Log(Level_Info, fmt.Sprintln(v...))
-}
-
-func (self *Logger) Warnln(v ...interface{}) {
-
-	self.Log(Level_Warn, fmt.Sprintln(v...))
-}
-
-func (self *Logger) Errorln(v ...interface{}) {
-	self.Log(Level_Error, fmt.Sprintln(v...))
-}
-
-func (self *Logger) SetLevelByString(level string) {
-
-	self.SetLevel(str2loglevel(level))
-
-}
-
-func (self *Logger) SetLevel(lv Level) {
-	self.level = lv
-}
-
-func (self *Logger) Level() Level {
-	return self.level
-}
-
-// 注意, 加色只能在Gogland的main方式启用, Test方式无法加色
-func (self *Logger) SetColorFile(file *ColorFile) {
-	self.colorFile = file
-}
 func (self *Logger) IsDebugEnabled() bool {
 	return self.level == Level_Debug
 }
--- part_name.go
+++ part_name.go
@@ -1,7 +1,7 @@
 package golog
 
-func LogPart_Level(log *Logger) {
-	log.WriteRawString(levelString[log.currLevel])
+func LogPart_CurrLevel(log *Logger) {
+	log.WriteRawString(log.CurrLevelString())
 	log.WriteRawByte(' ')
 
 }
